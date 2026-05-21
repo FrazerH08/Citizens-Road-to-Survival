a545243342e10ba4c851134edd02036813a573fd
Frazer Harness <654994@eastridingcollege.ac.uk>
2026-05-21 20:01:00 +0100

    mobile nav bar changes

--- nav.js
+++ nav.js
@@ -4,16 +4,19 @@ const hamburgerEl = document.querySelector('.hamburger');
 hamburgerEl.addEventListener('click', () => {
     navEl.classList.toggle('nav--open');
     hamburgerEl.classList.toggle('hamburger--open');
+    document.body.classList.toggle('menu-open');
 });
 
 navEl.addEventListener('click', () => {
     navEl.classList.remove('nav--open');
     hamburgerEl.classList.remove('hamburger--open');
+    document.body.classList.remove('menu-open');
 });
+
 
 // const myPara =document.getElementById("login");
 
 // myPara.innerHTML="<a 'class=nav_item' href=logout.php >Logout</a>";
 
-document.getElementById("login"). href="logout.php";
-document.getElementById("login"). a="Logout";
+document.getElementById("login").href = "logout.php";
+document.getElementById("login").a = "Logout";
